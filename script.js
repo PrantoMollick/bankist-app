71cbaf8dbbdbc9c233a6d326de1406bda281eb67
pranto mollick <prantocse01@gmail.com>
2023-01-17 17:30:33 +0600

    challenge 1 complete

--- script.js
+++ script.js
@@ -173,3 +173,22 @@ currenciesUnique.forEach((value, _, map) => {
   console.log(`Key: ${value}`);
 });
 */
+
+///////////////////////////////////////////////
+////Challenge #1
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  console.log(dogsJuliaCorrected);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  dogs.forEach(function (age, i) {
+    if (age < 3) console.log(`Dog number ${i + 1} is still a puppy🐶`);
+    else
+      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+  });
+};
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
