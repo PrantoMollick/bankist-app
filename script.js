ade0f53bd446a1d60b7713aa34dee12537922ec6
pranto mollick <prantocse01@gmail.com>
2023-01-18 14:29:05 +0600

    the reduce method

--- script.js
+++ script.js
@@ -82,6 +82,13 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcPrintBlance = function (movements) {
+  const blance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${blance} EUR`;
+};
+
+calcPrintBlance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(acc => {
     acc.username = acc.owner
@@ -94,7 +101,6 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 
-console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -240,3 +246,26 @@ console.log(depositsFor);
 
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
+
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+//Maximum value
+const max = movements.reduce((acc, curr, i, arr) => {
+  if (curr > acc) {
+    acc = curr;
+  }
+
+  return acc;
+}, 0);
+
+const max2 = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+
+console.log(max2);
